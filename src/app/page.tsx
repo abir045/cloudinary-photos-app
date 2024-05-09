4a2319dc865814a6d15fbf009fc459ca1790bd5f
unknown <abir045@gmail.com>
2024-05-09 13:04:18 +0600

    working on gallery page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,15 @@ import { CldUploadButton } from "next-cloudinary";
 import { CldImage } from "next-cloudinary";
 import { useState } from "react";
 
-type UploadResult = {
+export type UploadResult = {
   event: "success";
   info: {
     public_id: string;
   };
-};
+}; // the value currently in the public ID input field
 
 export default function Home() {
-  const [imageId, setImageId] = useState("");
+  const [imageId, setImageId] = useState("1518155317743-a8ff43ea6a5f");
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <CldUploadButton
@@ -26,6 +26,7 @@ export default function Home() {
           width="600"
           height="400"
           src={imageId}
+          cartoonify
           sizes="100vw"
           alt="Description of my image"
         />
